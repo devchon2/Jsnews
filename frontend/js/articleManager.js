62c9d4345e23caff7b9f8e87c12209aaa3b960af
rachon34 <rachon3441@gmail.com>
2023-03-09 05:48:50 +0100

    creation classe ArticleManager

--- frontend/js/articleManager.js
+++ frontend/js/articleManager.js
@@ -1,3 +1,19 @@
 /**
  * Gestion des articles en objet, gère le tri par date et le formatage des dates pour affichage
  */
+articles = fetch("http://localhost:4000/api/article").then(response=> response.json()).then((data) => console.log(data));
+
+
+
+class ArticleManager{
+    constructor(articles){
+        this.articles = articles;
+        console.log(this.articles);
+        this.articles.sort()
+        console.log(this.articles.sort())
+    }
+    
+     
+}
+
+  
