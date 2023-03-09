6a3629e6384c605c240e7b4b2bd549c622bcff67
rachon34 <rachon3441@gmail.com>
2023-03-09 02:25:24 +0100

    construction de la Classe Article

--- frontend/js/article.js
+++ frontend/js/article.js
@@ -1,3 +1,18 @@
 /**
  * Représentation du format d'un article ainsi que le formatage des dates pour affichage
  */
+class Article{
+    constructor(id, title, publicationDate, image,content){
+        this.id = id;
+        this.title = title;
+        this.publicationDate = new Date(publicationDate);
+        this.image = image;
+        this.content = content;
+    
+        const formatedPubDate = this.publicationDate.toLocaleDateString("fr-FR");
+        this.publicationDate = formatedPubDate;
+        
+    }
+    
+}
+
